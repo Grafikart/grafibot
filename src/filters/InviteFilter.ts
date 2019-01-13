2bfa798b89d7e4e4bc39c941875acde971d5f0c7
Jonthan Boyer <contact@grafikart.fr>
2019-01-13 11:21:47 +0100

    fix: regexp invite discord

--- src/filters/InviteFilter.ts
+++ src/filters/InviteFilter.ts
@@ -14,9 +14,13 @@ export default class InviteFilter implements IFilter {
   }
 
   filter (message: Message): boolean {
-    if (message.content.match(/(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/[0-9A-Za-z]*/i) !== null) {
-      this.muteCommand.muteMember(message.member, 'Les liens d\'invitation discord sont interdit sur ce serveur').catch(console.error)
-      message.delete().catch(console.error)
+    if (message.content.match(/(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/[0-9A-Za-z]+/i) !== null) {
+      this.muteCommand
+        .muteMember(message.member, 'Les liens d\'invitation discord sont interdit sur ce serveur')
+        .then(function () {
+          return message.delete()
+        })
+        .catch(console.error)
       return true
     }
 
