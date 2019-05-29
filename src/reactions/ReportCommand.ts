a079fcc815d92fa239ef0c9e2760897ba0061a62
Jonathan <contact@grafikart.fr>
2019-05-29 09:14:40 +0200

    fix: auteur du message

--- src/reactions/ReportCommand.ts
+++ src/reactions/ReportCommand.ts
@@ -17,7 +17,7 @@ export default class ReportCommand implements IReactionCommand {
     reaction.remove(user).catch(console.error)
     const modoRole = reaction.message.guild.roles.find(r => r.name === 'Modo')
     const permalink = `https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}`
-    this.logger.log(`${modoRole.toString()} <@!${reaction.message.author.id}> a signalé le message ${permalink}
+    this.logger.log(`${modoRole.toString()} <@!${user.id}> a signalé le message ${permalink}
 \`\`\`
 ${reaction.message.content}
 \`\`\``)
