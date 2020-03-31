4bd0f5428929a6441a744379029994e2b99b3f86
Jonthan Boyer <contact@grafikart.fr>
2020-03-31 09:24:57 +0200

    feat: citation message Rulecommand

--- src/reactions/RuleCommand.ts
+++ src/reactions/RuleCommand.ts
@@ -41,10 +41,13 @@ export default class RuleCommand implements IReactionCommand {
         reaction.remove(reaction.users.first())
         return null
       }
+      const permalink = `https://discordapp.com/channels/${
+        reaction.message.guild.id
+      }/${reaction.message.channel.id}/${reaction.message.id}`
       this.logger.log(
         `<@!${reaction.users.first().id}> a utilisé le bot pour la règle ${
           reaction.emoji.name
-        }`
+        } sur le message ${permalink}`
       )
       reaction.message.reply(
         `:robot: **règle ${reaction.emoji.name}** : ${message}`
