689092e36a54d31999e915b455c100b558a34abd
Jonathan <contact@grafikart.fr>
2022-01-28 17:22:43 +0100

    fix: intent GUILD_MEMBERS

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,6 @@ dotenv.config()
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
     Intents.FLAGS.GUILD_INTEGRATIONS,
     Intents.FLAGS.GUILD_MESSAGES,
