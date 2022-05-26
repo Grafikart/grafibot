c41c756674dfd5fb03e3a95aa857b710c3df22cd
Benjamin Noufel <benjamin.noufel@gmail.com>
2022-05-26 18:30:13 +0200

    Exclude smiley

--- src/filters/CapslockFilter.ts
+++ src/filters/CapslockFilter.ts
@@ -7,18 +7,23 @@ export default class CapslockFilter {
   filter (message: Message): boolean {
     if (this.isCapslock(message.content)) {
       message.channel
-        .send(`:scream_cat: Pas la peine de hurler <@!${message.author.id}>`)
-        .catch(console.error)
+          .send(`:scream_cat: Pas la peine de hurler <@!${message.author.id}>`)
+          .catch(console.error)
       return true
     }
     return false
   }
 
+  private isOnlySmiley(content: string): boolean {
+    return content.match(/^\<\:[A-Z0-9]+\:[0-9]+\>/gm) !== null
+  }
+
   private isCapslock (content: string): boolean {
     return (
-      content === content.toUpperCase() &&
-      content.length > 15 &&
-      content.match(/[A-Z]{4,}/) !== null
+        content === content.toUpperCase() &&
+        content.length > 15 &&
+        content.match(/[A-Z]{4,}/) !== null &&
+        !this.isOnlySmiley(content)
     )
   }
 }
